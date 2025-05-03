c9ebab72a6acf3bd819a8bdccd0233ef61bd0b47
takanaweb5 <50874513+takanaweb5@users.noreply.github.com>
2025-05-04 07:45:07 +0900

    仮

--- script.ts
+++ script.ts
@@ -1,4 +1,4 @@
-const DRAW_PER_FRAME = 5; // 1フレームで何セル分アニメーションさせるか
+const DRAW_PER_FRAME = 2; // 1フレームで何セル分アニメーションさせるか
 const numCols = 30;
 const numRows = 20;
 const cellSize = 16;
@@ -146,25 +146,26 @@ function getOneLineHintString(colOrRow: string, num: number): string {
   }
   return blackCounterArray.length > 0 ? blackCounterArray.join(',') : "0";
 }
-/**
- * ヒント配列を生成する関数
- * @param colOrRow - 'col' または 'row' の文字列
- * @param length - 行数 または 列数
- * @returns 生成されたヒント配列 例:['1,3,1','2,3']
- */
-function makeHintArray(colOrRow: string, length: number): string[] {
-  const result: string[] = [];
-  for (let i = 1; i <= length; i++) {
-    const hint = getOneLineHintString(colOrRow, i);
-    result.push(hint);
-  }
-  return result;
-}
 
 /**
  * ヒントを生成する
  */
 function makeHints() {
+  /**
+   * ヒント配列を生成する関数
+   * @param colOrRow - 'col' または 'row' の文字列
+   * @param length - 行数 または 列数
+   * @returns 生成されたヒント配列 例:['1,3,1','2,3']
+   */
+  function makeHintArray(colOrRow: string, length: number): string[] {
+    const result: string[] = [];
+    for (let i = 1; i <= length; i++) {
+      const hint = getOneLineHintString(colOrRow, i);
+      result.push(hint);
+    }
+    return result;
+  }
+
   //縦のヒントを作成
   const colHints = makeHintArray('col', numCols);
   //横のヒントを作成
@@ -245,7 +246,18 @@ function shuffleCells(cellsArr: HTMLDivElement[]) {
  * 行・列ごとにセルを塗りつぶし、全体が埋まるまで繰り返す。
  * @returns なし
  */
-async function solve(): Promise<void> {
+async function solve(colHints: number[][], rowHints: number[][]): Promise<void> {
+  // 複数の描画を一気にまとめてアニメーションする関数
+  const args: { row: number; col: number; color: number }[] = [];
+  function animate(args: { row: number; col: number; color: number }[]) {
+    return new Promise<void>(resolve => {
+      requestAnimationFrame(() => {
+        args.forEach(arg => cells[arg.row][arg.col].textContent = String(arg.color));
+        resolve();
+      });
+    });
+  }
+
   // 行と列の処理を共通化する関数
   // 1行または1列単位の処理を関数化
   async function processSingleRowOrCol(isRow: boolean, index: number) {
@@ -255,7 +267,7 @@ async function solve(): Promise<void> {
     // 未知セル収集
     for (let j = 1; j <= limit; j++) {
       const [row, col] = isRow ? [index, j] : [j, index];
-      if (!isCellFixed[row][col]) {
+      if (!massBoard[row][col]) {
         unknownCells.push(cells[row][col]);
       }
     }
@@ -274,24 +286,13 @@ async function solve(): Promise<void> {
       return aCol - bCol;
     });
 
-    // 複数の描画を一気にまとめてアニメーションする関数
-    const args: { row: number; col: number; color: number }[] = [];
-    function animate(args: { row: number; col: number; color: number }[]) {
-      return new Promise<void>(resolve => {
-        requestAnimationFrame(() => {
-          args.forEach(arg => cells[arg.row][arg.col].textContent = String(arg.color));
-          resolve();
-        });
-      });
-    }
-
     for (let k = 0; k < fillCount; k++) {
       const cell = sorted[k];
       const [row, col] = isRow ? [index, Number(cell.dataset.col)] : [Number(cell.dataset.row), index];
       const color = Math.random() < 0.5 ? 1 : 2;
 
-      if (!isCellFixed[row][col]) {
-        isCellFixed[row][col] = true;
+      if (massBoard[row][col] === 0) {
+        massBoard[row][col] = color;
         args.push({ row, col, color });
 
         // 数セルをまとめてアニメーションさせる(速度調整)
@@ -300,11 +301,6 @@ async function solve(): Promise<void> {
           args.length = 0;
         }
       }
-    }
-
-    // 残り処理
-    if (args.length > 0) {
-      await animate(args);
     }
   }
 
@@ -315,13 +311,13 @@ async function solve(): Promise<void> {
     }
   }
 
-  // solve用の2次元配列（1-indexedでcellsと同じ構造）
-  // 1-indexedで使うため0番目は未使用
-  const isCellFixed: boolean[][] = Array.from({ length: numRows + 1 }, () => Array(numCols + 1).fill(false));
-
   while (!isPuzzleComplete()) {
     await processCells(true);
     await processCells(false);
+    // 残り処理
+    if (args.length > 0) {
+      await animate(args);
+    }
   }
 }
 
@@ -333,16 +329,54 @@ if (title) {
 /**
  * 解析開始イベント
  */
-async function handleTitleClick(): Promise<void> {
-  // 事前準備
+function handleTitleClick() {
+  mainSolve();
+}
+
+/**
+ * 0列目または0行目からヒントの配列を取得する
+ * @param isRow - trueなら行ヒント、falseなら列ヒント
+ * @returns 例：[[1,3,1],[2,3]]
+ */
+function makeHintsList(isRow: boolean): number[][] {
+  const result: number[][] = [];
+  const limit = isRow ? numCols : numRows;
+  for (let index = 1; index <= limit; index++) {
+    const [row, col] = isRow ? [0, index] : [index, 0];
+    const cell = cells[row][col].textContent || '';
+    result.push(cell.split(',').map(Number));
+  }
+  return result;
+}
+
+async function mainSolve() {
+  const colHints = makeHintsList(true);
+  const rowHints = makeHintsList(false);
+
+  // 横と縦のヒントの合計が同じかどうかをチェック
+  const rowSum = rowHints.reduce((a, b) => a + b.reduce((c, d) => c + d, 0), 0);
+  const colSum = colHints.reduce((a, b) => a + b.reduce((c, d) => c + d, 0), 0);
+  if (rowSum !== colSum) {
+    window.alert(`横のヒントの合計${rowSum} ≠ 縦のヒントの合計${colSum} です`);
+    return;
+  }
+
+  // gridの初期化
   await new Promise<void>(resolve => requestAnimationFrame(() => {
     const oldMsg = document.getElementById('finish-msg');
     if (oldMsg) oldMsg.remove();
     clearGrid();
     resolve();
   }));
+
+    // solve用の2次元配列（1-indexedでcellsと同じ構造）
+  // 1-indexedで使うため0番目は未使用
+  const massBoard: number[][] = Array.from({ length: numRows + 1 },
+    () => Array(numCols + 1).fill(0));
+
+  // solve開始
   const startTime = Date.now();
-  await solve();
+  await baseSolve(massBoard, colHints, rowHints);
   const elapsedSec = ((Date.now() - startTime) / 1000).toFixed(1);
 
   // 新しいメッセージノードを作成
@@ -358,3 +392,179 @@ async function handleTitleClick(): Promise<void> {
   }
 }
 
+/**
+ * お絵かきロジックを背理法で解く
+ * すべての行と列に対して背理法を適用して、確定できるマスが0になるまで繰り返す
+ * @param massBoard - お絵かきロジックの状態（白,黒,未）を格納した2次元配列
+ * @param rowHints - すべての行のヒントを格納した配列
+ * @param colHints - すべての列のヒントを格納した配列
+ * @returns 確定件数、エラー時は-1
+ */
+async function baseSolve(massBoard: number[][], rowHints: number[][], colHints: number[][]): Promise<number> {
+  let result = 0;
+
+  // 確定できるマスがある間は繰り返す
+  let count = 99; // 確定したマスの数
+  while (count > 0) {
+    count = 0;
+
+    // 各行についてdetermineMassを実行
+    for (let y = 0; y < rowHints.length; y++) {
+      const hint = rowHints[y];
+      const blockList = massBoard[y];
+      const [cnt, updatedBlockList] = determineMass(hint, blockList);
+      if (cnt == -1) { // エラーの時
+        return -1;
+      }
+      if (cnt > 0) {
+        massBoard[y] = updatedBlockList;  // 更新
+        count += cnt;
+      }
+    }
+
+    // 各列についてdetermineMassを実行
+    for (let x = 0; x < colHints.length; x++) {
+      const hint = colHints[x];
+      const blockList = massBoard[x];
+      const [cnt, updatedBlockList] = determineMass(hint, blockList);
+      if (cnt == -1) { // エラーの時
+        return -1;
+      }
+      if (cnt > 0) {
+        massBoard[x] = updatedBlockList;  // 更新
+        count += cnt;
+      }
+    }
+    result += count;
+  }
+  return result;
+}
+
+/**
+ * いずれかの1行または1列について、対応するヒントに基づき
+ * ブロックを出来る限り左揃えにした配列を取得する
+ * @param hintList 行または列のヒントを格納した配列
+ * @param blockList 現在のブロック状態（白,黒,未）を格納した配列
+ * @returns 左揃えのブロック状態（白,黒,未）を格納した配列、エラー時はnull
+ */
+function leftAlignBlocks(hintList: number[], blockList: number[]): number[] | null {
+  const blockCount = hintList.length;
+  const massCount = blockList.length;
+
+  // 最も右端の黒マスの位置を保存（存在しない場合は0を設定）
+  const revList = [...blockList].reverse().indexOf(2);
+  const maxRightPos = revList !== -1 ? blockList.length - 1 - revList : 0;
+
+  // 各ブロックの右端と左端の位置を格納する配列を定義
+  const rightPos = Array.from({ length: blockCount }, () => 0);
+  const leftPos = Array.from({ length: blockCount }, () => 0);
+
+  // 最初(左端)のブロックの右端を設定
+  let k = 1;
+  rightPos[k] = hintList[k];
+
+  while (true) {
+
+    // 最後(最も右端)のブロックの時
+    if (k === blockCount) {
+      // もっと右に黒マスがあればその位置まで右側にシフト
+      if (rightPos[k] < maxRightPos)
+        rightPos[k] = maxRightPos;
+    }
+    // カレントブロックの左端を設定
+    leftPos[k] = rightPos[k] - hintList[k] + 1;
+
+    // カレントブロック内を右端から左端に向かって1文字ずつ照合
+    for (let i = rightPos[k]; i >= leftPos[k]; i--) {
+      if (blockList[i] === 1) {
+        // 白マスの右にカレントブロック全体をシフト
+        rightPos[k] = i + hintList[k];
+        if (rightPos[k] > massCount)
+          return null; // ブロックの右端がはみ出た
+        // カレントブロックをやり直し
+        continue; // 外側のループを続ける
+      }
+    }
+
+    // カレントブロックの左隣のマスから左側のブロックまで1文字ずつ照合
+    for (let i = leftPos[k] - 1; i >= (k > 1 ? rightPos[k - 1] + 1 : 1); i--) {
+      if (blockList[i] === 2) {
+        if (k === 1) {
+          return null; // 先頭ブロックのさらに左に黒マスあり
+        }
+        // 左側のブロックの右端を黒マスの位置まで右側にシフト
+        rightPos[k - 1] = i;
+        // 1つ前(左)のブロックをやり直し
+        k -= 1;
+        continue; // 外側のループを続ける
+      }
+    }
+
+    // 次(右)のブロックへ
+    k += 1;
+    if (k > blockCount) {
+      break;
+    }
+
+    // 次のブロックの右端を設定
+    rightPos[k] = rightPos[k - 1] + hintList[k] + 1;
+    if (rightPos[k] > massCount) {
+      return null; // ブロックの右端がはみ出た
+    }
+
+    continue;
+  }
+
+  // 結果の配列を作成
+  const outBlockList = Array.from({ length: massCount }, () => 1);
+  for (let k = 1; k <= blockCount; k++) {
+    if (leftPos[k] >= 1 &&
+      rightPos[k] >= 1 &&
+      leftPos[k] <= rightPos[k]) {
+      for (let i = leftPos[k]; i <= rightPos[k]; i++) {
+        outBlockList[i] = 2;
+      }
+    }
+  }
+  return outBlockList;
+}
+
+/**
+ * いずれかの1行または1列について、ヒントと現在のブロック状態に基づいて、
+ * 確定可能なマスを全て確定する
+ * 引数:
+ * @param hintList 行または列のヒントを格納した配列
+ * @param blockList 現在のブロック状態（白,黒,未）を格納した配列
+ * @returns Tuple
+ * - Int: 確定したマスの数、エラー時は-1
+ * - Vector{Int}: 確定可能なマスを全て確定した後の配列
+ */
+function determineMass(hintList: number[], blockList: number[]): [number, number[]] {
+  // ブロックを左寄せする
+  const leftAligned = leftAlignBlocks(hintList, blockList);
+  if (leftAligned === null) {
+    return [-1, []];
+  }
+  // ブロックを右寄せする
+  const rightAligned = leftAlignBlocks(hintList.reverse(), blockList.reverse())?.reverse() ?? [];
+
+  const result: number[] = blockList.slice()
+  const testBlocks: number[] = blockList.slice()
+  let counter = 0
+
+  for (let i = 1; i <= blockList.length; i++) {
+    if (blockList[i] === 1)
+      // 左寄せと右寄せで同じ値なら反対の色で背理法を実行
+      if (leftAligned[i] == rightAligned[i]) {
+        testBlocks[i] = leftAligned[i] == 2 ? 1 : 2
+
+        if (leftAlignBlocks(hintList, testBlocks) === null)
+          // 矛盾すれば仮定と反対の色で確定
+          result[i] = leftAligned[i]
+          counter += 1
+        // 元に戻す
+        testBlocks[i] = 1
+      }
+    }
+  return [counter, result]
+}
